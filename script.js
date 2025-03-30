0974b0e039c9fd88bd99405a1aaf764ba24c3dd5
Eduardo Luis Alonso <eduardoluisalonsi@gmail.com>
2025-03-30 14:53:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ function mostrarQuemSouEu() {
     var quemSouEuSection = document.getElementById("quem-sou-eu");
     var projetosSection = document.getElementById("projetos");
 
+    // Adiciona a classe para bloquear a rolagem
+    document.body.classList.add("no-scroll");
+
     apresentacaoSection.classList.remove("visible");
     projetosSection.style.display = "none";
 
     quemSouEuSection.style.display = "block";
     quemSouEuSection.classList.add("full-screen");
 
-    setTimeout(function() {
+    // Espera a seção ser exibida e então faz a transição suave no <p>
+    setTimeout(function () {
         quemSouEuSection.classList.add("visible");
     }, 10);
 }
@@ -19,12 +23,15 @@ function mostrarApresentacao() {
     var quemSouEuSection = document.getElementById("quem-sou-eu");
     var projetosSection = document.getElementById("projetos");
 
+    // Remove a classe para permitir a rolagem novamente
+    document.body.classList.remove("no-scroll");
+
     quemSouEuSection.classList.remove("visible");
     quemSouEuSection.classList.remove("full-screen");
     quemSouEuSection.style.display = "none";
     projetosSection.style.display = "none";
 
-    setTimeout(function() {
+    setTimeout(function () {
         apresentacaoSection.classList.add("visible");
     }, 10);
 }
@@ -34,17 +41,20 @@ function mostrarProjetos() {
     var quemSouEuSection = document.getElementById("quem-sou-eu");
     var projetosSection = document.getElementById("projetos");
 
+    // Remove a classe para permitir a rolagem novamente
+    document.body.classList.remove("no-scroll");
+
     apresentacaoSection.classList.remove("visible");
     quemSouEuSection.classList.remove("visible");
     quemSouEuSection.style.display = "none";
 
     projetosSection.style.display = "block";
-    setTimeout(function() {
+    setTimeout(function () {
         projetosSection.classList.add("visible");
     }, 10);
 }
 
-window.onload = function() {
+window.onload = function () {
     var apresentacaoSection = document.getElementById("apresentacao");
     apresentacaoSection.classList.add("visible");
 };
